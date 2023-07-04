300616b13f3253f50e89df9ce3c70ab28c742477
movyton <doberman9903@gmail.com>
2023-07-04 14:46:30 +0300

    modified

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import speechSettings from './speechSettings.js'
-import ErrorPage from './ErrorPage.jsx'
-import './index.css'
+import recognition from './Comps/APIs/speechSettings.js'
+import NotSupportedBrowser from './Comps/otherComps/NotSupportedBrowser.jsx'
+import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {!speechSettings ? <ErrorPage /> : <App />}
+    {!recognition ? <NotSupportedBrowser /> : <App />}
   </React.StrictMode>
 )
