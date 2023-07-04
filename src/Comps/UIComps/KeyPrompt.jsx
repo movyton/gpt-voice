1a99ec112744390d267635bed7aa3985d39110b4
movyton <doberman9903@gmail.com>
2023-07-04 14:42:15 +0300

    openai prompt created

---
+++ src/Comps/UIComps/KeyPrompt.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import Button from './Button'
+import { keyValidation } from '../../APIs/openai'
+import APIKeyProvider from '../../APIs/APIKeyProvider'
+
+const KeyPrompt = () => {
+  const [input, setInput] = useState(
+    !APIKeyProvider.getSecretKey ? '' : APIKeyProvider.getSecretKey
+  )
+  const [incorrectKeyMessage, setIncorrectKeyMessage] = useState(false)
+
+  const handleInputChange = (event) => {
+    setInput(event.target.value)
+  }
+
+  const keyVerif = (key) => {
+    const keyReq = keyValidation(key)
+    if (keyReq) {
+      setIncorrectKeyMessage(false)
+      return
+    }
+    setIncorrectKeyMessage(true)
+  }
+
+  return (
+    <div className="key-prompt">
+      <input
+        value={input}
+        onChange={handleInputChange}
+        type="text"
+        placeholder="your API key"
+      />
+      <Button
+        func={() => keyVerif(input)}
+        isRecording={false}
+        textContent={'save key'}
+      />
+      <p className={incorrectKeyMessage ? 'unvalid-key-notification' : 'hide'}>
+        invalid key
+      </p>
+    </div>
+  )
+}
+
+export default KeyPrompt
